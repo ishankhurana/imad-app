3c2d5afc7a062f2df7f666ad76c14f00354987bb
ishankhurana <30648078+ishankhurana@users.noreply.github.com>
2017-08-19 22:51:34 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -6,7 +6,7 @@ var element = document.getElementById('main text');
 element.innerHTML = 'hello beta';
 
 //moving the image
-var img = document.getElementById ('madi')
+var img = document.getElementById('madi')
 img.onclick = function () {
     img.style.marginLeft = '100px';
 };
